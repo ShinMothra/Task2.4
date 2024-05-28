5496e4003d66c7555647113542612c62f8cf07b9
ShinMothra <145927979+ShinMothra@users.noreply.github.com>
2024-05-28 07:26:57 +0300

    Delete db_credentials.php

--- db_credentials.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('DB_HOST', 'localhost'); // Имя хоста
-define('DB_USER', 'your_username'); // Имя пользователя базы данных
-define('DB_PASS', 'your_password'); // Пароль пользователя базы данных
-define('DB_NAME', 'your_database_name'); // Имя базы данных
-?>
